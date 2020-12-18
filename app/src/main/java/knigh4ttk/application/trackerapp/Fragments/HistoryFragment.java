315664f0518058006b3ea19924a616798f9315fb
Arsen Tokbaev <tokbaev_a@auca.kg>
2020-12-18 22:05:24 +0600

    Initial commit

---
+++ app/src/main/java/knigh4ttk/application/trackerapp/Fragments/HistoryFragment.java
@@ -0,0 +1,21 @@
+package knigh4ttk.application.trackerapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import knigh4ttk.application.trackerapp.R;
+
+public class HistoryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.history_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/knigh4ttk/application/trackerapp/MainActivity.java
@@ -0,0 +1,48 @@
+package knigh4ttk.application.trackerapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import knigh4ttk.application.trackerapp.Fragments.HistoryFragment;
+import knigh4ttk.application.trackerapp.Fragments.RunFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new RunFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.runFragment:
+                            selectedFragment = new RunFragment();
+                            break;
+                        case R.id.historyFragment:
+                            selectedFragment = new HistoryFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+}
